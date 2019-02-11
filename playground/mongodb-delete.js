708894c6e4f7d5b4f24f96babfcb14dc56cf4eb3
williamleonardo <william.leonardo@mitrais.com>
2019-02-11 08:43:36 +0700

    add delete function

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,23 @@
+const { MongoClient, ObjectID } = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server')
+    }
+
+    console.log('Connected to MongoDB server')
+
+    var db = client.db('TodoApp');
+
+    // db.collection('Todos').deleteMany({ text: 'Eat lunch' }).then((res) => {
+    //     console.log(res)
+    // })
+
+    // db.collection('Todos').deleteOne({ text: 'Eat lunch' }).then((res) => {
+    //     console.log(res)
+    // })
+
+    // db.collection('Todos').findOneAndDelete({ completed: false }).then((res) => {
+    //     console.log(res)
+    // })
+})
