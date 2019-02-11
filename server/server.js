c0cf257381936c1dcf6dab7412dce9517413d00c
williamleonardo <william.leonardo@mitrais.com>
2019-02-11 10:31:26 +0700

    add todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const { mongoose } = require('./db/mongoose')
+const { Todo } = require('./models/todo')
+const { User } = require('./models/user')
+
+var app = express()
+var port = 3000
+
+app.use(bodyParser.json())
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    })
+
+    todo.save().then((doc) => {
+        res.send(doc)
+    }, (e) => {
+        res.send(e)
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Started on port', port)
+})
