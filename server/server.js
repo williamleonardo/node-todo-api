8274dc32644f2892d172c3005591fac24d388e9c
williamleonardo <william.leonardo@mitrais.com>
2019-02-12 10:30:23 +0700

    add start script, remove playground, DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { Todo } = require('./models/todo')
 const { User } = require('./models/user')
 
 var app = express()
-var port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.post('/todos', (req, res) => {
@@ -38,6 +38,24 @@ app.get('/todos/:id', (req, res) => {
     }
 
     Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send()
+        }
+
+        res.send({todo})
+    }).catch((e) => {
+        res.status(400).send()
+    })
+})
+
+app.delete('/todos/:id', (req, res)=> {
+    var id = req.params.id
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send()
+    }
+
+    Todo.findByIdAndDelete(id).then((todo) => {
         if (!todo) {
             return res.status(404).send()
         }
