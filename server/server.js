4f476fb55fa049349f18dd1f875024b50acd1980
williamleonardo <william.leonardo@mitrais.com>
2019-02-13 14:10:01 +0700

    add seed for todos and users

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ var app = express()
 const port = process.env.PORT
 
 app.use(bodyParser.json())
+
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,23 +1,13 @@
-const { ObjectID } = require('mongodb')
 const expect = require('expect')
 const request = require('supertest')
+const { ObjectID } = require('mongodb')
 
 const { app } = require('./../server')
 const { Todo } = require('./../models/todo')
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed')
 
-const todos = [{
-    _id: new ObjectID(),
-    text: 'First test todo'
-}, {
-    _id: new ObjectID(),
-    text: 'Second test todo'
-}]
-
-beforeEach((done) => {
-    Todo.deleteMany({}).then(() => {
-        return Todo.insertMany(todos)
-    }).then(() => done())
-})
+beforeEach(populateUsers)
+beforeEach(populateTodos)
 
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
