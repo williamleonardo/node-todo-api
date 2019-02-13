a2792dfe601263d8427b3704cb7876f193cdde3e
williamleonardo <william.leonardo@mitrais.com>
2019-02-13 13:00:12 +0700

    hash password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const { SHA256 } = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-var data = {
-    id: 10
-}
+var password = '123abc!'
 
-var token = jwt.sign(data, '123abc')
-console.log(token)
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash)
+    })
+})
 
-var decoded = jwt.verify(token, '123abc')
-console.log('Decoded', decoded)
+var hashedPassword = '$2a$10$ErLH9mQFd4GUQcL7zZpbeO6iJSCD6s2kiIvisvjmFq5.VbfEbKu.e'
+
+bcrypt.compare('123abc!', hashedPassword, (err, res) => {
+    console.log(res)
+})
+
+// var data = {
+//     id: 10
+// }
+
+// var token = jwt.sign(data, '123abc')
+// console.log(token)
+
+// var decoded = jwt.verify(token, '123abc')
+// console.log('Decoded', decoded)
 
 // var message = "I am user number 3"
 // var hash = SHA256(message).toString()
