305f2fa3a020ddade735beab81a58127a0e50bee
williamleonardo <william.leonardo@mitrais.com>
2019-02-13 10:07:55 +0700

    add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -96,6 +96,20 @@ app.patch('/todos/:id', (req, res) => {
     })
 })
 
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password'])
+    var user = new User(body)
+
+    user.save().then((user) => {
+        return user.generateAuthToken()
+        // res.send(user)
+    }).then((token) => {
+        res.header('x-auth', token).send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
 app.listen(3000, () => {
     console.log('Started on port', port)
 })
