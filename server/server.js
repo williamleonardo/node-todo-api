12e20851905edce91610f8b3d26d2a9d031703ac
williamleonardo <william.leonardo@mitrais.com>
2019-02-13 10:31:04 +0700

    add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const { mongoose } = require('./db/mongoose')
 mongoose.set('useFindAndModify', false);
 const { Todo } = require('./models/todo')
 const { User } = require('./models/user')
+const {authenticate} = require('./middleware/authenticate')
 
 var app = express()
 const port = process.env.PORT
@@ -108,6 +109,10 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e)
     })
+})
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user)
 })
 
 app.listen(3000, () => {
